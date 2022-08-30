74718486ce3f80d373b90865bc162ac1a2e8c145
Sunny-unik <sunnygandhwani027@gmail.com>
2022-08-30 10:10:05 +0530

    add footer and update dasboard UI

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaGithub } from 'react-icons/fa';
+
+export default function Footer() {
+  return (
+    <footer className="py-4 bg-light mt-auto">
+      <div className="container-fluid px-4">
+        <div className="d-flex align-items-center justify-content-end small">
+          <details>
+            <summary className="text-muted">Copyright &copy; crud_page </summary>
+            <p> &middot; by <FaGithub /> <a target='blank' rel="noreferrer" href='https://github.com/Sunny-unik/'>
+              @Sunny-unik</a></p>
+          </details>
+        </div>
+      </div>
+    </footer>
+  )
+}
