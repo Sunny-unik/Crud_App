0f64700c766c574c78d48a2c6d0199394190845d
Sunny-unik <sunnygandhwani027@gmail.com>
2022-09-08 10:09:47 +0530

    update components in frontend for handle CRUD

--- client/src/components/CreateStudent.js
+++ client/src/components/CreateStudent.js
@@ -1,7 +1,237 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function CreateStudent() {
+export default function CreateStudent(props) {
+  const studentId = props.match.params.id;
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [age, setage] = useState("");
+  const [marks, setmarks] = useState("");
+  const [city, setcity] = useState("");
+  const [uploadPercentage, setuploadPercentage] = useState("");
+  let profile;
+
+  useEffect(() => {
+    if (studentId) {
+      axios
+        .get(process.env.REACT_APP_API_URL + "/student-by-id/?id=" + studentId)
+        .then(res => {
+          setname(res.data.data[0].name);
+          setemail(res.data.data[0].email);
+          setage(res.data.data[0].age);
+          setmarks(res.data.data[0].marks);
+          setcity(res.data.data[0].city);
+        });
+    }
+  }, [studentId]);
+
+  function setvalue(e) {
+    e.target.name === "name" && setname(e.target.value);
+    e.target.name === "age" && setage(e.target.value);
+    e.target.name === "email" && setemail(e.target.value);
+    e.target.name === "marks" && setmarks(e.target.value);
+    e.target.name === "city" && setcity(e.target.value);
+  }
+
+  function setprofile(e) {
+    profile = e.target.files[0];
+  }
+
+  function isValid() {
+    if (document.getElementById("inputName").value === "") {
+      alert("name can't be empty");
+      return false;
+    }
+    if (document.getElementById("inputEmail").value === "") {
+      alert("email can't be empty");
+      return false;
+    }
+    if (document.getElementById("inputAge").value === "") {
+      alert("age can't be empty");
+      return false;
+    }
+    if (document.getElementById("inputMarks").value === "") {
+      alert("marks can't be empty");
+      return false;
+    }
+    if (document.getElementById("inputCity").value === "") {
+      alert("city can't be empty");
+      return false;
+    }
+    submit();
+  }
+
+  function updateStudent() {
+    const formData = new FormData();
+    formData.append("_id", studentId);
+    formData.append("name", name);
+    formData.append("city", city);
+    formData.append("marks", marks);
+    formData.append("age", age);
+    formData.append("email", email);
+    formData.append("profile", profile);
+
+    if (formData.get("profile") === "undefined") {
+      alert("Please select a profile image");
+      return false;
+    }
+
+    axios
+      .post(process.env.REACT_APP_API_URL + "/update-student", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+        onUploadProgress: function(progressEvent) {
+          console.log("file Uploading Progresss....... ", progressEvent);
+          setuploadPercentage(
+            parseInt(
+              Math.round((progressEvent.loaded / progressEvent.total) * 100)
+            )
+          );
+          //setfileInProgress(progressEvent.fileName)
+        }
+      })
+      .then(res => {
+        alert(res.data.data);
+      })
+      .catch(res => {
+        alert("Sorry, server side error occurred");
+      });
+  }
+
+  function submit() {
+    if (studentId) {
+      updateStudent();
+      return false;
+    }
+
+    const studentObj = { name, age, email, marks, city };
+    axios
+      .post(process.env.REACT_APP_API_URL + "/create-student", studentObj)
+      .then(res => {
+        alert("Student Created Successfully");
+      })
+      .catch(res => {
+        alert(res.data.data);
+      });
+  }
+
   return (
-    <div>CreateStudent</div>
-  )
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-5">
+          <div className="card shadow-lg border-0 rounded-lg mt-0 ">
+            <div className="card-header py-0">
+              <h3 className="text-center font-weight-light my-4">
+                {studentId ? "Update Student" : "Create Student"}
+              </h3>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="form-group mb-3">
+                  <label htmlFor="inputName">Name</label>
+                  <input
+                    value={name}
+                    name="name"
+                    onChange={e => {
+                      setvalue(e);
+                    }}
+                    className="form-control"
+                    id="inputName"
+                    type="text"
+                    placeholder="Enter Student Name"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="inputEmail">Email address</label>
+                  <input
+                    name="email"
+                    onChange={e => {
+                      setvalue(e);
+                    }}
+                    value={email}
+                    className="form-control"
+                    id="inputEmail"
+                    type="email"
+                    placeholder="name@example.com"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="inputAge"> Age </label>
+                  <input
+                    name="age"
+                    onChange={e => {
+                      setvalue(e);
+                    }}
+                    className="form-control"
+                    value={age}
+                    id="inputAge"
+                    type="number"
+                    placeholder="Enter Student Age"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="inputMarks">Marks</label>
+                  <input
+                    name="marks"
+                    onChange={e => {
+                      setvalue(e);
+                    }}
+                    className="form-control"
+                    value={marks}
+                    id="inputMarks"
+                    type="number"
+                    placeholder="Enter Student Marks"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="inputCity">City</label>
+                  <select
+                    name="city"
+                    onChange={e => {
+                      setvalue(e);
+                    }}
+                    className="form-control"
+                    value={city}
+                    id="inputCity"
+                  >
+                    <option value="Jaipur">Jaipur</option>
+                    <option value="Pune">Pune</option>
+                    <option value="Mumbai">Mumbai</option>
+                    <option value="Goa">Goa</option>
+                    <option value="Delhi">Delhi</option>
+                  </select>
+                </div>
+
+                {!studentId ? (
+                  <hr />
+                ) : (
+                  <div className="form-group mb-3">
+                    <label htmlFor="profilepic">Profile Picture</label>
+                    <input
+                      name="profilepic"
+                      onChange={e => setprofile(e)}
+                      className="form-control"
+                      value={profile}
+                      type="file"
+                    />
+                    {uploadPercentage && uploadPercentage + "% uploaded"}
+                  </div>
+                )}
+
+                <div className="d-flex align-items-center justify-content-center mt-4 mb-0">
+                  <span
+                    className="btn btn-primary"
+                    onClick={() => {
+                      studentId ? submit() : isValid();
+                    }}
+                  >
+                    {studentId ? "Update Student" : "Create Student"}
+                  </span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/components/ListStudents.js
+++ client/src/components/ListStudents.js
@@ -1,7 +1,117 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
 
-export default function ListStudents() {
+export default function ListStundents(props) {
+  const [students, setstudents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/list-student")
+      .then(res => setstudents(res.data.data));
+  }, []);
+
+  function doAction(id, action) {
+    if (action === "update") {
+      props.history.push("/create-student/" + id);
+      return false;
+    }
+
+    const selectedStudentName = students.filter(st => st._id === id)[0].name;
+    if (
+      window.confirm("Do you wanna delete " + selectedStudentName + "'s data")
+    ) {
+      axios
+        .get(process.env.REACT_APP_API_URL + "/delete-student?id=" + id)
+        .then(res => alert(res.data.data));
+
+      axios
+        .get(process.env.REACT_APP_API_URL + "/list-student")
+        .then(res => setstudents(res.data.data));
+    }
+  }
+
   return (
-    <div>ListStudent</div>
-  )
+    <div className="container-fluid px-4">
+      <h1 className="mt-4">Student Table</h1>
+      <div className="card mb-4">
+        <div className="card-header">
+          <i className="fas fa-table me-1"></i>
+          DataTable
+        </div>
+        <div className="card-body">
+          <table id="datatablesSimple">
+            <thead className="table-head">
+              <tr>
+                <th>Profile Picture</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>City</th>
+                <th>Marks</th>
+                <th>Email</th>
+                <th colSpan="2">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="table-body">
+              {students.map(st => (
+                <tr key={st._id}>
+                  <td>
+                    <img
+                      className="user-profile"
+                      height="140px"
+                      width="110px"
+                      src={
+                        st.profile
+                          ? process.env.REACT_APP_API_URL + `/${st.profile}`
+                          : "logo192.png"
+                      }
+                      alt="user profile"
+                      loading="lazy"
+                    />
+                  </td>
+                  <td>{st.name}</td>
+                  <td>{st.age}</td>
+                  <td>{st.city}</td>
+                  <td>{st.marks}</td>
+                  <td>{st.email}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        doAction(st._id, "delete");
+                      }}
+                    >
+                      {" "}
+                      Delete{" "}
+                    </button>
+                    &nbsp;
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        doAction(st._id, "update");
+                      }}
+                    >
+                      {" "}
+                      Update{" "}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot className="table-foot">
+              <tr>
+                <th>Profile Picture</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>City</th>
+                <th>Marks</th>
+                <th>Email</th>
+                <th colSpan="2">Actions</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
